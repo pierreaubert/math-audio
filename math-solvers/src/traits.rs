d6de52232b49b3fc1b5133c1bed9e0d8427d3776
pierre aubert <pierre@spinorama.org>
2026-01-09 22:53:29 +0100

    added missing traits

--- math-solvers/src/traits.rs
+++ math-solvers/src/traits.rs
@@ -7,7 +7,7 @@
 
 use ndarray::Array1;
 use num_complex::{Complex32, Complex64};
-use num_traits::{Float, NumAssign, One, Zero};
+use num_traits::{Float, NumAssign, One, Zero, FromPrimitive, ToPrimitive};
 use std::fmt::Debug;
 use std::ops::Neg;
 
